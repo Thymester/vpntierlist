43ce693a65b9311adefd35c5da941bf08746f16d
Tyler <48775431+Thymester@users.noreply.github.com>
2023-08-26 19:10:37 -0400

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -31,8 +31,8 @@ const Header = () => {
         <div className="header-dropdown">
           Quizzes
           <div className="header-dropdown-content">
-            <Link to="https://www.vpntierlist.com/are-you-anonymous-quiz">Are you anonymous quiz?</Link>
-            <Link to="https://www.vpntierlist.com/vpn-quiz">VPN quiz?</Link>
+            <Link to="https://www.vpntierlist.com/are-you-anonymous-quiz" target="_blank" rel="noopener noreferrer">Are you anonymous quiz?</Link>
+            <Link to="https://www.vpntierlist.com/vpn-quiz" target="_blank" rel="noopener noreferrer">VPN quiz?</Link>
           </div>
         </div>
       </div>
