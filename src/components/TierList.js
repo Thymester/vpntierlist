c3ce5bb417d170188b177fc7c80fc991ca140711
Tyler <48775431+Thymester@users.noreply.github.com>
2024-04-14 17:57:51 -0400

    Update TierList.js

--- src/components/TierList.js
+++ src/components/TierList.js
@@ -5,10 +5,10 @@ const TierList = () => {
   const tiers = ['A', 'B', 'C', 'D', 'Blacklist'];
 
   const vpnData = {
-    A: ['TorGuard (82.25)', 'SurfShark (77)', 'NordVPN (76.25)', 'Hide.Me (75)', 'ProtonVPN (72)'],
-    B: ['Private Internet Access (68.5)', 'VyprVPN (62.5)', 'ExpressVPN (61)'],
-    C: ['IvacyVPN (59)', 'PrivateVPN (58.5)', 'CactusVPN (56.5)', 'Mullvad (56)', 'PureVPN (56)', 'AtlasVPN (55)', 'Cyberghost (55)', 'AdGuard (55)', 'AirVPN (51.5)', 'AzireVPN (50)'],
-    D: ['IPVanish (49)', 'oVPN (46)', 'VPN Unlimited (44)', 'Zenmate (47.5)', 'HolaVPN (45.5)', 'HideIPVPN (43.5)', 'BraveVPN (32)', 'AvastVPN (24)'],
+    A: ['NordVPN (82.25)', 'TorGuard (80.25)', 'Hide.Me (80)', 'SurfShark (79)', 'ProtonVPN (79)', 'PIA (71.5)'],
+    B: ['VyprVPN (62.5)', 'ExpressVPN (61)', 'PersonalVPN (61)],
+    C: ['IvacyVPN (60)', 'PrivateVPN (59.5)', 'CactusVPN (57.5)', 'Mullvad (57)', 'PureVPN (57)', 'AtlasVPN (56)', 'Cyberghost (56)', 'AdGuard (56)', 'AirVPN (52.5)', 'AzireVPN (51)'],
+    D: ['IPVanish (50)', 'HideIPVPN (49.5)', 'Zenmate (48.5)', 'oVPN (48.5)', 'VPN Unlimited (45)', 'HolaVPN (46.5)', 'BraveVPN (33)', 'AvastVPN (25)'],
     Blacklist: ['Windscribe (62)'],
   };
 
